1098ed0164a492b066257ebacd2d2f742b6be56c
Chris Duzan <cgduzan@gmail.com>
2023-04-23 12:11:41 -0400

    🔧 Fix ESM & Prettier issues on /server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+import { books } from './data.js'
+
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -20,17 +22,6 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-]
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
